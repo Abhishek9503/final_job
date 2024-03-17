7e200dda0bfca0599dd6bb378d190c389c8615dc
Abhishek <abhishek.pub9503@gmail.com>
2024-03-17 13:53:10 +0530

    text changes

--- frontend/src/components/Auth/Login.jsx
+++ frontend/src/components/Auth/Login.jsx
@@ -1,3 +1,109 @@
+// import React, { useContext, useState } from "react";
+// import { MdOutlineMailOutline } from "react-icons/md";
+// import { RiLock2Fill } from "react-icons/ri";
+// import { Link, Navigate } from "react-router-dom";
+// import { FaRegUser } from "react-icons/fa";
+// import axios from "axios";
+// import toast from "react-hot-toast";
+// import { Context } from "../../main";
+
+// const Login = () => {
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [role, setRole] = useState("");
+
+//   const { isAuthorized, setIsAuthorized } = useContext(Context);
+
+//   const handleLogin = async (e) => {
+//     e.preventDefault();
+//     try {
+//       const { data } = await axios.post(
+//         "https://final-job.onrender.com/api/v1/user/login",
+//         { email, password, role },
+//         {
+//           headers: {
+//             "Content-Type": "application/json",
+//           },
+//           withCredentials: true,
+//         }
+//       );
+//       toast.success(data.message);
+//       setEmail("");
+//       setPassword("");
+//       setRole("");
+//       setIsAuthorized(true);
+//       localStorage.setItem("token", data.token);
+//     } catch (error) {
+//       toast.error(error.response.data.message);
+//     }
+//   };
+
+//   if (isAuthorized) {
+//     return <Navigate to={"/"} />;
+//   }
+
+//   return (
+//     <>
+//       <section className="authPage">
+//         <div className="container">
+//           <div className="header">
+//             <img src="/JobZeelogo.png" alt="logo" />
+//             <h3>Login to your account</h3>
+//           </div>
+//           <form>
+//             <div className="inputTag">
+//               <label>Login As</label>
+//               <div>
+//                 <select value={role} onChange={(e) => setRole(e.target.value)}>
+//                   <option value="">Select Role</option>
+//                   <option value="Employer">Employer</option>
+//                   <option value="Job Seeker">Job Seeker</option>
+//                 </select>
+//                 <FaRegUser />
+//               </div>
+//             </div>
+//             <div className="inputTag">
+//               <label>Email Address</label>
+//               <div>
+//                 <input
+//                   type="email"
+//                   placeholder="xyz@gmail.com"
+//                   value={email}
+//                   onChange={(e) => setEmail(e.target.value)}
+//                 />
+//                 <MdOutlineMailOutline />
+//               </div>
+//             </div>
+//             <div className="inputTag">
+//               <label>Password</label>
+//               <div>
+//                 <input
+//                   type="password"
+//                   placeholder="Your Password"
+//                   value={password}
+//                   onChange={(e) => setPassword(e.target.value)}
+//                 />
+//                 <RiLock2Fill />
+//               </div>
+//             </div>
+//             <button type="submit" onClick={handleLogin}>
+//               Login
+//             </button>
+//             <Link to={"/register"}>Register Now</Link>
+//           </form>
+//         </div>
+//         <div className="banner">
+//           <img src="/login.png" alt="login" />
+//         </div>
+//       </section>
+//     </>
+//   );
+// };
+
+// export default Login;
+
+
+
 import React, { useContext, useState } from "react";
 import { MdOutlineMailOutline } from "react-icons/md";
 import { RiLock2Fill } from "react-icons/ri";
@@ -18,7 +124,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "https://final-job.onrender.com/api/v1/user/login",
+        "http://localhost:4000/api/v1/user/login",
         { email, password, role },
         {
           headers: {
@@ -32,11 +138,12 @@ const Login = () => {
       setPassword("");
       setRole("");
       setIsAuthorized(true);
-      localStorage.setItem("token", data.token);
     } catch (error) {
       toast.error(error.response.data.message);
     }
   };
+
+  
 
   if (isAuthorized) {
     return <Navigate to={"/"} />;
@@ -45,6 +152,9 @@ const Login = () => {
   return (
     <>
       <section className="authPage">
+        <div className="banner">
+          <img src="/login.png" alt="login" />
+        </div>
         <div className="container">
           <div className="header">
             <img src="/JobZeelogo.png" alt="logo" />
@@ -52,30 +162,42 @@ const Login = () => {
           </div>
           <form>
             <div className="inputTag">
-              <label>Login As</label>
+              <label
+                style={{ display: "flex", alignItems: "center", gap: "0.3rem" }}
+              >
+                {" "}
+                <FaRegUser /> Login As
+              </label>
               <div>
                 <select value={role} onChange={(e) => setRole(e.target.value)}>
                   <option value="">Select Role</option>
                   <option value="Employer">Employer</option>
                   <option value="Job Seeker">Job Seeker</option>
                 </select>
-                <FaRegUser />
               </div>
             </div>
             <div className="inputTag">
-              <label>Email Address</label>
+              <label
+                style={{ display: "flex", alignItems: "center", gap: "0.3rem" }}
+              >
+                <MdOutlineMailOutline /> Email Address
+              </label>
               <div>
                 <input
                   type="email"
-                  placeholder="xyz@gmail.com"
+                  placeholder="taskmaster991@gmail.com"
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
                 />
-                <MdOutlineMailOutline />
               </div>
             </div>
             <div className="inputTag">
-              <label>Password</label>
+              <label
+                style={{ display: "flex", alignItems: "center", gap: "0.3rem" }}
+              >
+                {" "}
+                <RiLock2Fill /> Password
+              </label>
               <div>
                 <input
                   type="password"
@@ -83,17 +205,24 @@ const Login = () => {
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
                 />
-                <RiLock2Fill />
               </div>
             </div>
-            <button type="submit" onClick={handleLogin}>
+            <button type="submit" onClick={handleLogin} style={{backgroundColor: "blue"}}>
               Login
             </button>
-            <Link to={"/register"}>Register Now</Link>
+
+            {/* <Link to={"/register"}>Register Now</Link> */}
+
+
+            <Link 
+    to={"/register"} 
+    style={{ backgroundColor: 'blue', color: 'black', padding: '10px 20px', textDecoration: 'none', borderRadius: '5px', transition: 'background-color 0.3s' }} 
+    onMouseEnter={(e) => e.target.style.backgroundColor = 'grey'} 
+    onMouseLeave={(e) => e.target.style.backgroundColor = 'white'}
+>
+    Register Now
+</Link>
           </form>
-        </div>
-        <div className="banner">
-          <img src="/login.png" alt="login" />
         </div>
       </section>
     </>
